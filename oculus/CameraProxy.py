6b723b258914057f58e7ab6e08213f405350b4af
Sze wun Wong <sww002@ubuntu.(none)>
2015-04-25 17:37:31 -0400

    new Camera Proxy

--- oculus/CameraProxy.py
+++ oculus/CameraProxy.py
@@ -2,15 +2,16 @@ import socket, sys, threading, time, os, signal
 
 # ros imports
 import rospy
-from std_msg.msg import String
+from std_msgs.msg import String, Bool
+from sensor_msgs.msg import Image
 
 def callback(data):
-	rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+	rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.height)
 
 def main():
 	rospy.init_node('CameraProxy', anonymous=True)
-	rospy.Subcriber("/wide_stereo/left/image_color", String, callback)
-	#rospy.spin()
+	rospy.Subscriber("/wide_stereo/left/image_color", Image, callback)
+	rospy.spin()
 
 
 if __name__ == "__main__":
